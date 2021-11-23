671d91503b7641d829e72b3596bbd704a1e5bf46
Bhuvanachandra26 <bhuvanachandra26@gmail.com>
2021-11-23 14:35:51 +0530

    Added : kajuBarfi

---
+++ src/com/bridgelabz/sweetshopmanagement/KajuBarfi.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.sweetshopmanagement;
+
+import java.util.ArrayList;
+
+public class KajuBarfi extends Sweet{
+	
+	public KajuBarfi() {
+		shape = "diamond";
+		colour = "white";
+		ingredient =new ArrayList();
+		ingredient.add("sugar");
+		ingredient.add("kaju");
+		ingredient.add("ghee");
+		name = "KajuBarfi";
+	}
+}
